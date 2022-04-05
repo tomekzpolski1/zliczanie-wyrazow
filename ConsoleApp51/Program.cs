de94acf05e70501a005b4387fa0cf88a440efc18
tomekzpolski1 <tomasz.rozynski92@gmail.com>
2022-04-05 12:13:05 +0200

    Add project files.

---
+++ ConsoleApp51/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp51
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj tekst");
+            string tekst = Console.ReadLine();
+            int liczbawyrazow = 1;
+            foreach (char litera in tekst)
+            {
+                if ( litera == ' ')
+                {
+                    liczbawyrazow++;
+                }
+            }
+            Console.WriteLine("Liczba wyrazów w tekscie to : {0}", liczbawyrazow );
+        }
+    }
+}
